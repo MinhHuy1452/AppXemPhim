03dd1688e9687302029b9ec98aa2009577dd768f
MinhHuy1452 <129708422+MinhHuy1452@users.noreply.github.com>
2024-06-28 20:01:12 +0700

    android

---
+++ app_xemphim/app_xemphim/app/src/main/java/com/example/app_xemphim/Fragment/ChangePasswordFragment.java
@@ -0,0 +1,52 @@
+package com.example.app_xemphim.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.app_xemphim.R;
+
+public class ChangePasswordFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public ChangePasswordFragment() {
+    }
+//'newInstance'la một phương thức tĩnh của lớp 'ChangePasswordFragment'
+//nó dùng để tạo một phiên bản mới của 'ChangePasswordFragment'
+//và có tham số truyền vào fragment đó là 'param1','param2'
+    public static ChangePasswordFragment newInstance(String param1, String param2) {
+        ChangePasswordFragment fragment = new ChangePasswordFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_change_password, container, false);
+
+
+        return view;
+    }
+}
